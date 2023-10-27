da9f464e1f6b9a8f7c975ca2e1ccaed6a803d458
Edwin Rudy Quisbert Montalvo <edwinquisbert.m@umss.edu>
2023-10-27 17:40:16 -0400

    Estructura de un plugin

---
+++ plugin-registro.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Plugin Name:       Sansi IA
+ * Plugin URI:        http://localhost/
+ * Description:       Este plugin sirve para conectar el Bot de Sansi a los sitios web en WordPress.
+ * Version:           1.0.0
+ * Requires at least: 6.1
+ * Requires PHP:      7.4
+ * Author:            Edwin Quisbert, Omar Balderrama
+ * Author URI:        https://github.com/edwinquisbertm
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       my-basics-plugin
+ * Domain Path:       /languages
+ */
